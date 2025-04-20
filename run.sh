b0c2dfd83883f98ee84ccbcd984c6b59002f6338
Kartik0806 <93075526+Kartik0806@users.noreply.github.com>
2025-04-20 22:11:40 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -13,9 +13,9 @@ python3  train_math.py \
     --bf16 True \
     --output_dir $SAVE_PATH \
     --num_train_epochs 1 \
-    --per_device_train_batch_size 16 \
+    --per_device_train_batch_size 2 \
     --per_device_eval_batch_size 4 \
-    --gradient_accumulation_steps 1 \
+    --gradient_accumulation_steps 2 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
     --save_steps 1000 \
